1af5e82c1e21b186d582c4a4a57f0a6a0f23b78a
arshadm101 <104524067+arshadm101@users.noreply.github.com>
2023-10-03 22:46:55 +0500

    m332

--- src/component/BrandCollaps.js
+++ src/component/BrandCollaps.js
@@ -1,3 +1,4 @@
+import next from 'next';
 import Link from 'next/link'
 import Image from 'next/image'
 import { Container,Col,Row, } from "react-bootstrap";
@@ -24,11 +25,11 @@ const BrandCollaps = () => {
   
     const getImageSource = () => {
       if (isClicked) {
-        return '/public/images/collaps1.webp';
+        return "/public/images/collaps1.webp";
       } else if (isHovered) {
-        return '/public/images/collaps1.webp';
+        return "/public/images/collaps1.webp";
       } else {
-        return '/public/images/collaps1.webp';
+        return "/public/images/collaps1.webp";
       }
     };
 
@@ -41,54 +42,59 @@ const BrandCollaps = () => {
                                 <h4 className="h4-font">Based in San Francisco, we strengthen brands through exceptional digital experiences for companies worldwide.</h4>
                             </Col>
                             <Col lg={2}>
-                            <div>
-                                <Image src={getImageSource()} alt="Image" width={400} height={400}/>
-                            </div>
-                        </Col>
-                        <Col lg={4} className="brands">
+                                <div>
+                                    <Image src={getImageSource()} alt="Image" width={400} height={400}/>
+                                </div>
+                            </Col>
+                            <Col lg={4} className="brands">
                                 <Accordion defaultActiveKey="0" flush>
-                                        <Accordion.Item eventKey="0">
+                                    <Accordion.Item eventKey="0">
                                             <Accordion.Header onMouseEnter={handleHover}
                                             onMouseLeave={handleHover}
                                             onClick={handleClick}>Branding</Accordion.Header>
                                             <Accordion.Body>
-                                                <p>A brand is more than just a logo and color palette. We'll craft a distinct visual and verbal identity, create necessary assets, and establish clear brand guidelines to maintain consistency across all
+                                                <p>
+                                                    A brand is more than just a logo and color palette. We will craft a distinct visual and verbal identity, create necessary assets, and establish clear brand guidelines to maintain consistency across all
                                                     touchpoints. </p>
                                             </Accordion.Body>
-                                        </Accordion.Item>
-                                        <Accordion.Item eventKey="1">
+                                    </Accordion.Item>
+                                    <Accordion.Item eventKey="1">
                                             <Accordion.Header  onMouseEnter={handleHover}
                                             onMouseLeave={handleHover}
                                             onClick={handleClick}>Digital Products</Accordion.Header>
                                             <Accordion.Body>
-                                                <p>Clay concentrates on designing authentic connections through the use of both aesthetics and behavioral science. Our team of senior UI/UX designers delivers engaging digital products that help brands thrive in the modern economy.</p>
+                                                <p>
+                                                    Clay concentrates on designing authentic connections through the use of both aesthetics and behavioral science. Our team of senior UI/UX designers delivers engaging digital products that help brands thrive in the modern economy.</p>
                                             </Accordion.Body>
-                                        </Accordion.Item>
-                                        <Accordion.Item eventKey="2" onMouseEnter={handleHover}
+                                    </Accordion.Item>
+                                    <Accordion.Item eventKey="2" onMouseEnter={handleHover}
                                             onMouseLeave={handleHover}
                                             onClick={handleClick}>
                                             <Accordion.Header>Websites</Accordion.Header>
                                             <Accordion.Body>
-                                                <p>A modern brand is defined by its digital presence, starting with the website. We focus on creating websites that effectively convey the brand's identity and perspective for an enhanced user experience.</p>
+                                                <p>
+                                                    A modern brand is defined by its digital presence, starting with the website. We focus on creating websites that effectively convey the brand's identity and perspective for an enhanced user experience.</p>
                                             </Accordion.Body>
-                                        </Accordion.Item>
-                                        <Accordion.Item eventKey="3" onMouseEnter={handleHover}
+                                    </Accordion.Item>
+                                    <Accordion.Item eventKey="3" onMouseEnter={handleHover}
                                             onMouseLeave={handleHover}
                                             onClick={handleClick}>
                                             <Accordion.Header>Development</Accordion.Header>
                                             <Accordion.Body>
-                                                <p>Our web developers are committed to achieving both visual and performance excellence. From the backend to the frontend, we prioritize delivering a seamless and enjoyable user experience on all devices.
+                                                <p>
+                                                    Our web developers are committed to achieving both visual and performance excellence. From the backend to the frontend, we prioritize delivering a seamless and enjoyable user experience on all devices.
                                                 </p>
                                             </Accordion.Body>
-                                        </Accordion.Item>
+                                    </Accordion.Item>
                                     <Accordion.Item eventKey="4" onMouseEnter={handleHover}
-                                            onMouseLeave={handleHover}
-                                            onClick={handleClick}>
-                                        <Accordion.Header>Content</Accordion.Header>
-                                        <Accordion.Body>
-                                            <p>Content is a crucial aspect of the user experience, and we offer a range of services to support this, including copywriting, illustration, 2D and 3D graphics, iconography, animation, video production, and photography.
-                                            </p>
-                                        </Accordion.Body>
+                                                onMouseLeave={handleHover}
+                                                onClick={handleClick}>
+                                            <Accordion.Header>Content</Accordion.Header>
+                                            <Accordion.Body>
+                                                <p>
+                                                    Content is a crucial aspect of the user experience, and we offer a range of services to support this, including copywriting, illustration, 2D and 3D graphics, iconography, animation, video production, and photography.
+                                                </p>
+                                            </Accordion.Body>
                                     </Accordion.Item>
                                 </Accordion>
                             </Col>
--- src/component/ImageWithShader.js
+++ src/component/ImageWithShader.js
@@ -1,28 +1,28 @@
-// components/ImageWithShader.js
-import React, { useState } from 'react';
-import ThreeScene from './ThreeScene';
+// // components/ImageWithShader.js
+// import React, { useState } from 'react';
+// import ThreeScene from './ThreeScene';
 
-const ImageWithShader = () => {
-  const [isHovered, setIsHovered] = useState(false);
+// const ImageWithShader = () => {
+//   const [isHovered, setIsHovered] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
+//   const handleMouseEnter = () => {
+//     setIsHovered(true);
+//   };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+//   const handleMouseLeave = () => {
+//     setIsHovered(false);
+//   };
 
-  return (
-    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <Image
-        src=""
-        alt="Your Image"
-        className={isHovered ? 'shader-hovered' : ''}
-      />
-      {isHovered && <ThreeScene />}
-    </div>
-  );
-};
+//   return (
+//     <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+//       <Image
+//         src=""
+//         alt="Your Image"
+//         className={isHovered ? 'shader-hovered' : ''}
+//       />
+//       {isHovered && <ThreeScene />}
+//     </div>
+//   );
+// };
 
-export default ImageWithShader;
+// export default ImageWithShader;
